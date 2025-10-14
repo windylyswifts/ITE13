92fd3e5f1f713cae3fc584232bcfed0304d77292
windylyswifts <isiahalinab@gmail.com>
2025-10-15 00:15:17 +0800

    finished activity 5

--- A5/activity5.c
+++ A5/activity5.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-void addStudent(int *count, int account[][50]);
-
-
 struct Student {
     int id;
     char name[50];
@@ -11,51 +8,111 @@ struct Student {
     int year;
 };
 
+void addStudent(int *count, struct Student *account);
+void displayStudents(int *count, struct Student *account);
+void searchStudent(int *count, struct Student *account);
 
+int main() {
+    struct Student account[50];
+    int checker, count = 0;
 
-int main(){
-    int checker, count = 0, account[50][50];
-    do{
+    do {
         printf("\n======Students======\n");
-        printf("1.Add student\n");
-        printf("2.Display student\n");
-        printf("3.Search Student\n");
-        printf("4.Exit\n");
+        printf("1. Add student\n");
+        printf("2. Display student\n");
+        printf("3. Search Student\n");
+        printf("4. Exit\n");
         scanf("%d", &checker);
 
-        switch (checker){
+        switch (checker) {
         case 1:
+            if (count >= 50) {
+                printf("ERROR, EXCEEDED 50 STUDENTS\n");
+                continue;
+            }
             addStudent(&count, account);
             break;
         case 2:
-            
+            displayStudents(&count, account);
+            break;
+        case 3:
+            searchStudent(&count, account);
         default:
             break;
         }
     } while (checker != 4);
-    
-    
+
     return 0;
 }
 
-
-void addStudent(int *count, int account[][50]){
+void addStudent(int *count, struct Student *account) {
+    int addCount;
+        printf("How many students you'd be adding? ");
+        scanf("%d", &addCount);
+    if (addCount > 50){
+        printf("ERROR too much student");
+        return;
+    }
     
 
-    //this is when the the system doesn't have a student yet 
-    if (*count == 0){
-        printf("How many student you'd be adding?");
-        scanf("%d", count);
+    for (int i = *count; i < *count + addCount; i++) {
+        printf("Enter the %d student ID: ", i + 1);
+        scanf("%d", &account[i].id); 
+        //since next is a character input, it might read things that aren't supposed to be read
+        getchar();
+        printf("Student's name:");
+        fgets(account[i].name, 50, stdin);
+        account[i].name[strcspn(account[i].name, "\n")] = '\0';
+
+        printf("Enter Student's Course:");
+        fgets(account[i].course, 50, stdin);
+        account[i].course[strcspn(account[i].course, "\n")] = '\0';
+
+        printf("Student's Year Level:");
+        scanf("%d", &account[i].year);
+
+        printf("Student added Succesfully\n\n");
     }
 
-    
+    //this prepares for the next batch 
+    *count += addCount;
+}
+
+void displayStudents(int *count, struct Student *account){
+    if (*count == 0){
+        printf("there is no student yet");
+        return;
+    }
 
     for (int i = 0; i < *count; i++){
-        printf
+        printf("Student number %d:\n", i+1);
+        printf("Student ID: %d\n", account[i].id);
+        printf("Name: %s\n", account[i].name);
+        printf("Course: %s\n", account[i].course);
+        printf("Year: %d\n", account[i].year);
     }
     
+}
 
-    
-    
+void searchStudent(int *count, struct Student *account){
+    int temp = 0, found = 0;
+    printf("Enter Student's ID:");
+    scanf("%d", &temp);
 
+    for (int i = 0; i < *count; i++){
+        if (account[i].id == temp){
+            printf("\nFound ID!\n");
+            printf("Student number %d:\n", i+1);
+            printf("Student ID: %d\n", account[i].id);
+            printf("Name: %s\n", account[i].name);
+            printf("Course: %s\n", account[i].course);
+            printf("Year: %d\n", account[i].year);
+            found++;
+        }        
+    }
+
+    if (found == 0){
+        printf("Couldn't find the ID!\n");
+        printf("I suggest displaying all student to find it manually\n");
+    }
 }
